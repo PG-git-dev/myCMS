145ecfa76b434b69286b05cc5a7d167384ea8ee3
PG-git-dev <lmd05@mail.ru>
2019-09-05 13:48:47 +0300

    Added class SitePage

---
+++ App_Code/SitePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Класс html-страницы Сводное описание для SitePage
+/// </summary>
+public class SitePage
+{
+    public string Name { get; set; }
+    public string Favicon { get; set; }
+    public int BlocksNumber { get; set; }
+
+    public SitePage()
+    {
+        
+
+     }
+}
